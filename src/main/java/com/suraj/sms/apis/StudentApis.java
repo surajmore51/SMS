4672667edede826488a31feb442b8fc250227b2b
Suraj More <Suraj More@ho.rnt.ai>
2024-11-26 15:15:28 +0530

    Api changes

--- src/main/java/com/suraj/sms/apis/StudentApis.java
+++ src/main/java/com/suraj/sms/apis/StudentApis.java
@@ -1,15 +1,19 @@
 package com.suraj.sms.apis;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.suraj.sms.dto.StudentDto;
 import com.suraj.sms.services.StudentService;
@@ -37,5 +41,13 @@ public class StudentApis {
 	public ResponseEntity<Map<String, Object>> addStudent(@RequestBody StudentDto studentDto) {
 
 		return studentService.addStudent(studentDto);
+	}
+	
+	@PostMapping("uploadFile")
+	public ResponseEntity<Map<String, Object>> uploadFile(@RequestParam("file") MultipartFile multipartFile){
+		System.out.println("File name"+multipartFile.getOriginalFilename());
+		Map<String, Object> map = new HashMap<>();
+		map.put("fie name:", multipartFile.getOriginalFilename());	
+		return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
 	}
 }
